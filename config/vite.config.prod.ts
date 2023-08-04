abcdcc9733fe95bf57522679dafdb86caf8dd158
chenzx <qchenzexuan@vip.qq.com>
2023-08-04 21:59:09 +0800

    解决本地和线上样式不一致

--- config/vite.config.prod.ts
+++ config/vite.config.prod.ts
@@ -2,7 +2,7 @@ import { mergeConfig } from 'vite';
 import baseConfig from './vite.config.base';
 import configCompressPlugin from './plugin/compress';
 import configVisualizerPlugin from './plugin/visualizer';
-import configArcoResolverPlugin from './plugin/arcoResolver';
+// import configArcoResolverPlugin from './plugin/arcoResolver';
 import configImageminPlugin from './plugin/imagemin';
 
 export default mergeConfig(
@@ -11,7 +11,7 @@ export default mergeConfig(
     plugins: [
       configCompressPlugin('gzip'),
       configVisualizerPlugin(),
-      configArcoResolverPlugin(),
+      // configArcoResolverPlugin(),
       configImageminPlugin(),
     ],
     build: {
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
+// Styles are imported via arco-plugin. See config/plugin/arcoStyleImport.ts in the directory for details
+// 样式通过 arco-plugin 插件导入。详见目录文件 config/plugin/arcoStyleImport.ts
+// https://arco.design/docs/designlab/use-theme-package
+import '@/assets/style/global.less';
+import '@/api/interceptor';
+
 import { createApp } from 'vue';
 import ArcoVue from '@arco-design/web-vue';
 import ArcoVueIcon from '@arco-design/web-vue/es/icon';
@@ -7,11 +13,6 @@ import store from './store';
 import i18n from './locale';
 import directive from './directive';
 import App from './App.vue';
-// Styles are imported via arco-plugin. See config/plugin/arcoStyleImport.ts in the directory for details
-// 样式通过 arco-plugin 插件导入。详见目录文件 config/plugin/arcoStyleImport.ts
-// https://arco.design/docs/designlab/use-theme-package
-import '@/assets/style/global.less';
-import '@/api/interceptor';
 
 const app = createApp(App);
 
